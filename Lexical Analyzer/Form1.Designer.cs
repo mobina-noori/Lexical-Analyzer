4a0c37fba482201a762407d7f0f03f523694e7ba
Mobina <mobina.noori61@gmail.com>
2024-07-18 18:43:45 +0330

    first

---
+++ Lexical Analyzer/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace Lexical_Analyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(359, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 64);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(359, 279);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Flie Path";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Source Code";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button1.Location = new System.Drawing.Point(377, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 88);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Open";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.button3.Location = new System.Drawing.Point(377, 197);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 88);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Exit";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(481, 9);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(552, 334);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 354);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.BindingSource bindingSource1;
+    }
+}
+
---
+++ Lexical Analyzer/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lexical_Analyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var sKeywords = new List<string>(){
+                "using","import","include","asm","else","auto","bool","break","case","catch","char","class","const","const_cast",
+                "continue","default","delete","do","double","dynamic_cast","else","enum","explicit",
+                "export","extern","false","float","for","friend","goto","if","inline","int","long",
+                "main","mutable","namespace","new","operator","private","protected","public",
+                "register","reinterpret_cast","return","short","signed","sizeof","static","cout",
+                "static_cast","struct","switch","template","this","throw","true","try","typedef",
+                "typeid","typename","union","unsigned","using","virtual","void","void","volatile","wchar_t","while"};
+            OpenFileDialog fd = new OpenFileDialog();
+            List<String> list = new List<String>();
+            List<String> list1 = new List<String>();
+            fd.Title = "Choose a source code";
+            fd.Filter = "txt files (*.txt)|*.txt|cpp files(*.cpp)|*.cpp";
+            fd.FilterIndex = 3;
+            fd.RestoreDirectory = true;
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fd.FileName;
+            }
+            string[] lines = System.IO.File.ReadAllLines(@textBox1.Text);
+            foreach (string li in lines)
+            {
+                richTextBox1.AppendText(li + "\n");
+            }
+            foreach (string li in lines)
+            {
+                list.Add(li);
+            }
+            int blokeNumber = 0;
+            richTextBox2.AppendText("Token" + "      " + "Type" + "                " + "Row" + "             " + "Column" + "        " + "Bloke Number" + "\n");
+            for (int i = 0; i < list.Count; i++)
+            {
+
+                string temp = "";
+                int j = 0;
+                foreach (Char ch in list[i])
+                {
+                    if (ch != ' ')
+                    {
+                        temp = temp + ch;
+
+                    }
+                    j++;
+                    if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '%' || ch == '&' || ch == '^'
+                    || ch == '<' || ch == '>' || ch == '!' || ch == '=')
+                    {
+                        richTextBox2.AppendText(ch + "              " + "operator" + "               " + (i + 1) + "                " + j + "             " + blokeNumber + "\n");
+                    }
+                    else if (ch == '{' || ch == '}' || ch == ';' || ch == ',' || ch == '(' || ch == ')')
+                    {
+                        if (ch == '{')
+                        {
+                            blokeNumber++;
+                        }
+                        richTextBox2.AppendText(ch + "              " + "Delimiters" + "                " + (i + 1) + "                " + j + "             " + blokeNumber + "\n");
+                    }
+                    else
+                    {
+                        foreach (String KeyW in sKeywords)
+                        {
+                            if (temp == KeyW)
+                            {
+                                temp = "";
+                                richTextBox2.AppendText(KeyW + "              " + "Key word" + "                " + (i + 1) + "                " + j + "             " + blokeNumber + "\n");
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
